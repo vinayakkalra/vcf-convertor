3fae5be8689dd0a603e2fcb80fc64a8d8ecd8a5e
DragonAaryaDot <chandan.mca.2019@gmail.com>
2020-09-17 11:55:48 +0530

    add new changes

--- download.php
+++ download.php
@@ -4,7 +4,7 @@ $c=$_SESSION['array_name'];
 if (!isset($_SESSION['authenticated']))
 {
     //if the value was not set, you redirect the user to your login page
-header('location:index');
+header('location:/');
 exit;
     
 }
@@ -24,7 +24,7 @@ else{
         var take=document.getElementById('server_text').value;                                    
         var blob = new Blob([take], {type: "text/plain;charset=utf-8"});
         saveAs(blob, "contact.vcf");
-        window.location.replace("index");               
+        window.location.replace("/");               
 </script>  
 </body>
 </html>
