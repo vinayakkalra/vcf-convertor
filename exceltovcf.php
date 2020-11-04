7c6cc4ea170b5aeccbee53c3732413cead27435e
DragonAaryaDot <chandan.mca.2019@gmail.com>
2020-11-04 10:55:06 +0530

    skip step 2

--- exceltovcf.php
+++ exceltovcf.php
@@ -1809,10 +1809,10 @@ session_start();
                     //submit step 2 by dekstop
                     $("#submit2_desktop").click(function() {
                         var sheet = $("#sheet").val();
-                        var first_column = $("#first_column").val();
-                        var last_column = $("#last_column").val();
-                        // var first_row = $("#first_row").val();
-                        // var last_row = $("#last_row").val();
+                        // var first_column = $("#first_column").val();
+                        // var last_column = $("#last_column").val();
+                        //  var first_row = 1;
+                        //  var last_row = total_data_length;
                         var show_data = "";
                         var error = "";                     
 
@@ -1877,7 +1877,7 @@ session_start();
                                     // var j=(i.charCodeAt(0)-97);
                                     total_data_come = number_data;
                                     console.log(total_data_come);
-                                    var show_data = Object.keys(JSON.parse(json_object)[0])[i - 1]; //give alphabet indexing 
+                                    var show_data = Object.keys(JSON.parse(json_object)[0])[i - 1]; //give  indexing 
 
                                     /* --------------------------------- testing -------------------------------- */
 
@@ -1894,7 +1894,7 @@ session_start();
                                         ((i + 9).toString(36).toUpperCase()) + ' .      What is ' +
                                         show_data +
                                         ' : </label><div class="col-7"><select name="last_column" id="column' +
-                                        (i - 64) +
+                                        (i - 1) +
                                         '" required=""class="form-control"><option value=""> -- select an option -- </option> <option value="first_name">First Name</option> <option value="last_name">Last Name</option> <option value="email">Email (Work)</option> <option value="mobile">Mobile No.</option> <option value="tel_office">Profession</option> <option value="tel_home">Position</option> <option value="fax">Department</option> <option value="company">Company Name</option> <option value="city">About</option> <option value="nickname">Nickname</option> <option value="address">Address (Home)</option><option value="address_work">Address (Office)</option><option value="website">Website</option><option value="email_personal">Email (Personal)</option><option value="phone_home">Tel (Home)</option><option value="phone_office">Tel (Office)</option></select></div>'
                                     );
                                     // console.log(String.fromCharCode(i));
@@ -1909,10 +1909,10 @@ session_start();
                     //submit step 2 by mobile
                     $("#submit2_mobile").click(function() {
                         var sheet = $("#sheet_mobile").val();
-                        var first_column = $("#first_column_mobile").val();
-                        var last_column = $("#last_column_mobile").val();
-                        var first_row = $("#first_row_mobile").val();
-                        var last_row = $("#last_row_mobile").val();
+                        // var first_column = $("#first_column_mobile").val();
+                        // var last_column = $("#last_column_mobile").val();
+                        // var first_row = $("#first_row_mobile").val();
+                        // var last_row = $("#last_row_mobile").val();
                         var show_data = "";
                         var error = "";
                         // if (first_row == "") {
@@ -1933,35 +1933,35 @@ session_start();
                         // }
                         /* ----------------------------- row bound check mobile ---------------------------- */
 
-                        var row_check_start = parseInt(first_row, 10);
-                        var row_check_end = parseInt(last_row, 10);
-                        if (row_check_start > total_data_length) {
-                            $("#first_row_mobile").css('border-color', 'red');
-                            $("#first_row_mobile").css('border-width', '2px');
-                            error = error + 'first_row';
-                            $('#alert_id_total_data_mobile').css('display', 'block');
-                            $('#alert_id_total_data_mobile').html("You have total "+(total_data_length-1)+" data");
-                        } else {
-                            $("#first_row_mobile").css('border-color', '#C0BBBB');
-                            $("#first_row_mobile").css('border-width', '1px');
-                            $('#alert_id_total_data_mobile').css('display', 'none');
-                        }
-                        if (row_check_end > total_data_length) {
-                            $("#last_row_mobile").css('border-color', 'red');
-                            $("#last_row_mobile").css('border-width', '2px');
-                            error = error + 'last_row';
-                            $('#alert_id_total_data_mobile').css('display', 'block');
-                            $('#alert_id_total_data_mobile').html("You have total "+(total_data_length-1)+" data");
-                        } else {
-                            $("#last_row_mobile").css('border-color', '#C0BBBB');
-                            $("#last_row_mobile").css('border-width', '1px');
+                        // var row_check_start = parseInt(first_row, 10);
+                        // var row_check_end = parseInt(last_row, 10);
+                        // if (row_check_start > total_data_length) {
+                        //     $("#first_row_mobile").css('border-color', 'red');
+                        //     $("#first_row_mobile").css('border-width', '2px');
+                        //     error = error + 'first_row';
+                        //     $('#alert_id_total_data_mobile').css('display', 'block');
+                        //     $('#alert_id_total_data_mobile').html("You have total "+(total_data_length-1)+" data");
+                        // } else {
+                        //     $("#first_row_mobile").css('border-color', '#C0BBBB');
+                        //     $("#first_row_mobile").css('border-width', '1px');
+                        //     $('#alert_id_total_data_mobile').css('display', 'none');
+                        // }
+                        // if (row_check_end > total_data_length) {
+                        //     $("#last_row_mobile").css('border-color', 'red');
+                        //     $("#last_row_mobile").css('border-width', '2px');
+                        //     error = error + 'last_row';
+                        //     $('#alert_id_total_data_mobile').css('display', 'block');
+                        //     $('#alert_id_total_data_mobile').html("You have total "+(total_data_length-1)+" data");
+                        // } else {
+                        //     $("#last_row_mobile").css('border-color', '#C0BBBB');
+                        //     $("#last_row_mobile").css('border-width', '1px');
                             
-                        }
+                        // }
                         /* --------------------------- row bound check end mobile-------------------------- */
                         if (error == "") {
                             $('.preloader_mobile').css('display', 'block');
-                            row_start = first_row;
-                            row_end = last_row;
+                            row_start = 1;
+                            row_end = total_data_length;
                             if (json_object_length >= sheet) {
                                 sheet = workbook.SheetNames[sheet - 1];
                                 var sheetName = sheet;
@@ -1982,23 +1982,22 @@ session_start();
                                     string_raw = "";
                                 }
                                 // console.log("first_column="+first_column);
+                                var number_data=Object.keys(JSON.parse(json_object)[0]).length;
                                 // console.log("last_column="+last_column);
-                                for (var i = first_column.charCodeAt(0); i <= last_column
-                                    .charCodeAt(0); i++) {
+                                for (var i = 1; i <= number_data; i++) {
                                     // var j=(i.charCodeAt(0)-97);
-                                    total_data_come = (last_column.charCodeAt(0) - 64);
-                                    var show_data = Object.keys(JSON.parse(json_object)[0])[
-                                        i - 65];
+                                    total_data_come = (number_data);
+                                    var show_data = Object.keys(JSON.parse(json_object)[0])[i - 1];
                                     if (show_data == undefined) {
                                         show_data = "";
                                     }
                                     $("#step3_dyanamic_data_mobile").html($(
                                             "#step3_dyanamic_data_mobile").html() +
                                         '<div class="form-group row"><label for="what_is_last_column" class="col"> Column ' +
-                                        String.fromCharCode(i) + ' .      What is ' +
+                                        ((i + 9).toString(36).toUpperCase()) + ' .      What is ' +
                                         show_data +
                                         ' : </label></div><div class="form-group row"><div class="col"><select name="last_column" id="column_mobile' +
-                                        (i - 64) +
+                                        (i - 1) +
                                         '" required=""class="form-control"><option value=""> -- select an option -- </option> <option value="first_name">First Name</option> <option value="last_name">Last Name</option> <option value="email">Email (Work)</option> <option value="mobile">Mobile No.</option> <option value="tel_office">Profession</option> <option value="tel_home">Position</option> <option value="fax">Department</option><option value="company">Company Name</option> <option value="city">About</option> <option value="nickname">Nickname</option><option value="address">Address (Home)</option><option value="address_work">Address (Office)</option> <option value="website">Website</option><option value="email_personal">Email (Personal)</option><option value="phone_home">Tel (Home)</option><option value="phone_office">Tel (Office)</option></select></div>'
                                     );
                                     // console.log(String.fromCharCode(i));
